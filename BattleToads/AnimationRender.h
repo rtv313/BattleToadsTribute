527b139ad24d118bfb189e6d952b52e03fac4894
Raul Treviño <rtv031.itc@gmail.com>
2016-12-21 17:30:09 +0100

    Eliminate Ghost in render and using defines

--- BattleToads/AnimationRender.h
+++ BattleToads/AnimationRender.h
@@ -5,6 +5,9 @@
 #include "Application.h"
 #include "ModuleRender.h"
 
+#define NUMBER_FRAMES_PER_SPRITE  9
+#define VALID_RENDER_FRAMES 10
+
 class AnimationRender {
 
 public:
@@ -21,18 +24,23 @@ public:
 	void Play(Application* const App, SDL_Texture* const graphics, Animation* const animation,bool const flipHorizontal,iPoint const position)
 	{
 		
-		if (animationCounter == 9 && actualOffset + 1 < animation->frames.size()) {
+		if (animationCounter >= NUMBER_FRAMES_PER_SPRITE && actualOffset + 1 < animation->frames.size()) {
 			++actualOffset;
 			animationCounter = 0;
 		}
 		
 
 		if (flipHorizontal) { // when looks left
-			App->renderer->Blit(graphics, position.x - offsetLeft[actualOffset].x, position.y - offsetLeft[actualOffset].y, &(animation->GetCurrentFrame()), 0.1f, flipHorizontal); // punche sprite
+			if (animationCounter < VALID_RENDER_FRAMES) // clean ghost
+				App->renderer->Blit(graphics, position.x - offsetLeft[actualOffset].x, position.y - offsetLeft[actualOffset].y, &(animation->GetCurrentFrame()), 0.1f, flipHorizontal); // punche sprite
+			else
+				&(animation->GetCurrentFrame()); // move frames to finish but we not render to clean ghost
 		}
 		else { // if looks right
-			
-			App->renderer->Blit(graphics, position.x, position.y- offsetRight[actualOffset].y, &(animation->GetCurrentFrame()), 0.1f, flipHorizontal);
+			if (animationCounter < VALID_RENDER_FRAMES) // clean ghost
+				App->renderer->Blit(graphics, position.x, position.y - offsetRight[actualOffset].y, &(animation->GetCurrentFrame()), 0.1f, flipHorizontal);
+			else
+				&(animation->GetCurrentFrame()); // move frames to finish but we not render to clean ghost
 		}
 		
 		++animationCounter; 
