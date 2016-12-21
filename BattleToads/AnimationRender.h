251e7d0cd051c181df76748ac4cdd1633e26998f
Raul Treviño <rtv031.itc@gmail.com>
2016-12-21 16:55:51 +0100

    Correct sprite with offset fixed

--- BattleToads/AnimationRender.h
+++ BattleToads/AnimationRender.h
@@ -21,18 +21,25 @@ public:
 	void Play(Application* const App, SDL_Texture* const graphics, Animation* const animation,bool const flipHorizontal,iPoint const position)
 	{
 		
-		actualOffset = animationCounter / 10;
+		if (animationCounter == 9 && actualOffset + 1 < animation->frames.size()) {
+			++actualOffset;
+			animationCounter = 0;
+		}
+		
 
 		if (flipHorizontal) { // when looks left
 			App->renderer->Blit(graphics, position.x - offsetLeft[actualOffset].x, position.y - offsetLeft[actualOffset].y, &(animation->GetCurrentFrame()), 0.1f, flipHorizontal); // punche sprite
 		}
 		else { // if looks right
+			
 			App->renderer->Blit(graphics, position.x, position.y- offsetRight[actualOffset].y, &(animation->GetCurrentFrame()), 0.1f, flipHorizontal);
 		}
 		
 		++animationCounter; 
+
 		if (animation->Finished()) {
 			animationCounter = 0;
+			actualOffset = 0;
 			return;
 		}
 	}
