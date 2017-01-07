d3d24dee9294e2e2d2b609307e4a3d65f708f8df
Raul Treviño <rtv031.itc@gmail.com>
2017-01-07 15:20:36 +0100

    Module Enemies Basic Structure done

--- BattleToads/ModuleEnemies.cpp
+++ BattleToads/ModuleEnemies.cpp
@@ -10,3 +10,31 @@ ModuleEnemies::ModuleEnemies()
 ModuleEnemies::~ModuleEnemies()
 {
 }
+
+
+update_status ModuleEnemies::PreUpdate() 
+{
+	
+	return UPDATE_CONTINUE;
+}
+
+update_status ModuleEnemies::Update() 
+{
+
+	return UPDATE_CONTINUE;
+}
+
+bool ModuleEnemies::CleanUp()
+{
+	return true;
+}
+
+void ModuleEnemies::DebugDraw()
+{
+
+}
+
+bool ModuleEnemies::Start() 
+{
+	return true;
+}
--- BattleToads/ModuleEnemies.h
+++ BattleToads/ModuleEnemies.h
@@ -8,15 +8,40 @@
 #include "Animation.h"
 #include "ModuleTextures.h"
 #include "AnimationRender.h"
+#include "ModuleCollision.h"
+
+class Enemy 
+{
+public:
+	int life;
+	int attack;
+	SDL_Rect rect = { 0,0,0,0 };
+	void Update();
+	Enemy();
+	~Enemy();
+private:
+	void Walk();
+	void Run();
+	void Attack();
+	void UnderAttack();
+	void Die();
+};
 
 class ModuleEnemies :
 	public Module
 {
 public:
-	SDL_Rect rect = { 0,0,0,0 };
+	
 	bool to_delete = false;
 	ModuleEnemies();
 	~ModuleEnemies();
+	update_status PreUpdate();
+	update_status Update();
+	Enemy* AddSpawnEnemy(const int  x, const int  y, const int  h, const int  w);
+	bool CleanUp();
+	void DebugDraw();
+	bool Start();
+
 };
 
 #endif
